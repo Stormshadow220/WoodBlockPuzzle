f79d52eef1394bce630f797ad9e03d137538b372
Stormshadow220 <bjoern.johannisson@gmx.de>
2018-06-09 13:57:51 +0200

    solving problem with isBlocked = 2 while adding blocks to an position in an unempty field, where u should not add something. field-backup works now and dont allow any cell.isblocked in field to become 2 anymore. But the block, who created the wrong adding still get removed.
    Additionally we started wrinting tests and funktions for the controller.

--- src/main/scala/de/htwg/se/woodblockpuzzle/controller/Controller.scala
+++ src/main/scala/de/htwg/se/woodblockpuzzle/controller/Controller.scala
@@ -1,18 +1,18 @@
 package de.htwg.se.woodblockpuzzle.controller
 import de.htwg.se.woodblockpuzzle.model.{Block, Field}
 case class Controller {
-  var field: Field
-  var b1 : Block
-  var b2 : Block
-  var b3 : Block
+  var field: Field = field(1)
+  var b1: Block = Block(0)
+  var b2: Block = Block(0)
+  var b3: Block = Block(0)
   var availableBlocks = 0
 
-  def createField: Field ={
+  def createField: Field = {
     field = Field(8)
   }
 
 
-  def createBlock(x:Int):Block={
+  def createBlock(x: Int): Block = {
     var b = Block(x)
     return b
   }
@@ -23,5 +23,23 @@ case class Controller {
     b2 = createBlock(r.nextInt(17))
     b3 = createBlock(r.nextInt(17))
     availableBlocks = 3
+  }
+
+
+  def addingBlock(blocknumber: Int, atx: Int, aty: Int): Unit = {
+
+  }
+
+  def showBlock(blocknumber: Int): String ={
+    var s : String
+    match(blocknumber){
+      case 1=> s =b1.toString
+      case 2=> s =b2.toString
+      case 3=> s =b3.toString
+    }
+
+  }
+  def showField(): String ={
+    return this.field.toString
   }
 }
--- src/test/scala/de/htwg/se/woodblockpuzzle/model/ControllerSpec.scala
+++ src/test/scala/de/htwg/se/woodblockpuzzle/model/ControllerSpec.scala
@@ -1,19 +1,21 @@
 package de.htwg.se.woodblockpuzzle.controller
 import de.htwg.se.woodblockpuzzle.model.{Block, Field}
+import de.htwg.se.woodblockpuzzle.controller.Controller
 import org.scalatest._
 
 
 class ControllerSpec extends WordSpec with Matchers{
   "A controller" when {
-    var controller = Controller
+    val controller1 = Controller
     "new" should {
       "create nothing, unless u used the right funktion" in{
-        controller
+        controller1
+        controller1
       }
     }
     "should create 3 new blocks with random blocktype between 0 and 17" in{
-      controller
       "after using create3RandomBlocks"
+      controller1.
 
     }
 
