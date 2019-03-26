82ef3ac7998128789a27be96d4250c089e8e18a8
Björn Johannisson <bjoern.johannisson@gmx.de>
2019-03-26 13:02:10 +0100

    Try to solve repaint vor BlockPanel

--- src/main/scala/de/htwg/se/woodblockpuzzle/aview/swing/BlockPanel.scala
+++ src/main/scala/de/htwg/se/woodblockpuzzle/aview/swing/BlockPanel.scala
@@ -8,7 +8,6 @@ import scala.swing._
 import scala.swing.event.MouseClicked
 
 class BlockPanel(controller: Controller) extends GridPanel(1,3){
-  listenTo(controller)
   border = LineBorder(java.awt.Color.BLACK, 2)
   background = java.awt.Color.darkGray
   preferredSize = new Dimension(300,120)
@@ -16,6 +15,7 @@ class BlockPanel(controller: Controller) extends GridPanel(1,3){
   contents += getLabel(2)
   contents += getLabel(3)
 
+  listenTo(controller)
 
   reactions += {
     case e: FieldChanged => {
@@ -24,6 +24,10 @@ class BlockPanel(controller: Controller) extends GridPanel(1,3){
   }
   def redraw = {
     print("Blocks: "+controller.availableBlocks+"\n")
+    contents.clear
+    //contents += getLabel(1)
+    //contents += getLabel(2)
+    //contents += getLabel(3)
     repaint
   }
 
@@ -31,10 +35,9 @@ class BlockPanel(controller: Controller) extends GridPanel(1,3){
     var label = new Label() {
       preferredSize = new Dimension(100,100)
       icon = new ImageIcon("src/main/scala/de/htwg/se/woodblockpuzzle/aview/swing/blockImages/b"+controller.getBlockType(i)+".png")
-      print("Label "+i+": b"+ controller.getBlockType(i)+".png")
+      println("Label "+i+": b"+ controller.getBlockType(i)+".png")
     }
     return label
   }
 
 }
-
